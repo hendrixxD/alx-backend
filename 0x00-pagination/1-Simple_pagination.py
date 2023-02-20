598409b805b50632c4a88ee054747c572728bfac
hendrixxD <maticsjnr@gmail.com>
2023-02-21 00:06:37 +0100

    checked

--- 0x00-pagination/1-Simple_pagination.py
+++ 0x00-pagination/1-Simple_pagination.py
@@ -1,5 +1,5 @@
-#!/usr/bin/env python3
-""" task 1 """
+#!/usr/bin/env pytthon3
+""" task """
 
 import csv
 import math
@@ -9,11 +9,10 @@ from typing import List
 def index_range(page: int, page_size: int) -> tuple:
     """ function returns a tuple of size two
     containing a start index and an end index"""
+    start_page_index = page_size * (page - 1)
+    end_page_size_index = start_page_index + page_size
 
-    end_index = page * page_size
-    start_index = end_index - page_size
-
-    return (start_index, end_index)
+    return (start_page_index, end_page_size_index)
 
 
 class Server:
@@ -36,13 +35,24 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """func that takes 2 arguments: page and page_size"""
-        assert type(page) is int and page > 0
-        assert type(page_size) is int and page_size > 0
+        """
+        page: page number
+        page_size: number of rows per page
+        """
 
-        self.dataset()
+        assert (page > 0 and isinstance(page, int))
+        assert (page_size > 0 and type(page_size) is int)
+        pass
+
+        start_page_index = page_size * (page - 1)
+        end_page_size_index = start_page_index + page_size
+
+        self.__dataset()
         if self.__dataset is None:
             return []
+
+        # for i in range(page_size):
+        #    print(list(self._dataset))
 
         index_range_tuple = index_range(page, page_size)
         data_range = self.__dataset[index_range_tuple[0]:index_range_tuple[1]]
