f5eb2a8b95e4e04f79b132a319f37377b94488db
hendrixxD <maticsjnr@gmail.com>
2023-02-22 16:23:04 +0100

    FIFO

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+FIFO caching
+"""
+
+from base_chaching import BaseCaching
+
+
+class FIFOCache(BaseCache):
+    """
+    FIFO Caching
+    """
+
+    def __init__(self):
+        """
+        constructor function
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """
+        assigns self.cache_data to item
+        """
+        if key is None or item is None:
+            pass
+        else:
+            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                last = self.cache_data.popitem()
+                print(f'DISCARD: {last[0]}')
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        function to get
+        """
+        if key is None and key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data.get(key)
