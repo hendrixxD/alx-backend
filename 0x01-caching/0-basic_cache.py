a693dd5ed5cfd808983f36232f6578b354cfe4f1
hendrixxD <maticsjnr@gmail.com>
2023-02-22 12:08:58 +0100

    Basic Dic

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+"""
+Basic dictionary
+"""
+
+BaseChaching = __import__('BaseCaching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """
+    Base class: BaseCaching
+    """
+
+    def __init__(self):
+        """
+        initialize the class using
+        __init__ parent class
+        """
+        BaseCaching.__init__(self)
+
+    def put(self, key, item):
+        """
+        assigns dic self.cache_Data
+        the item value for the key
+        """
+        if key or item is None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """
+        args:
+         key
+        """
+        if key is None or key not in self.cache_data:
+            return None
+        else:
+            return self.cache_data[key]
