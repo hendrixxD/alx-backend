d5800432bd2af8901d816c3d3ff5f5007a95d54c
hendrixxD <maticsjnr@gmail.com>
2023-03-01 09:08:38 +0100

    pypy

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -4,9 +4,11 @@ from flask import FLask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
     return render_template('index.html')
+
 
 if __naem__ == '__main__':
     app.run(debug=True)
